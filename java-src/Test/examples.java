34b7507aea850b55408372f1635e8d653e607fbc
YiQi Hu <huyq@lamda.nju.edu.cn>
2016-12-09 21:46:12 +0800

    implement Sequential RACOS (SRACOS)

--- java-src/Test/examples.java
+++ java-src/Test/examples.java
@@ -1,6 +1,10 @@
 package Test;
 
-import Racos.Componet.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import Racos.Component.*;
 import Racos.Method.*;
 import Racos.ObjectiveFunction.*;
 import Racos.Tools.RandomOperator;
@@ -8,33 +12,21 @@ import Racos.Tools.RandomOperator;
 public class examples {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
 		// parameters of Racos
-		int samplesize = 100;       // parameter: the number of samples in each iteration
-		int iteration = 10000;     // parameter: the number of iterations
+		int samplesize = 20;       // parameter: the number of samples in each iteration
+		int iteration = 100;       // parameter: the number of iterations for batch racos
+		int budget = 2000;         // parameter: the budget of sampling for sequential racos
 		int positivenum = 1;       // parameter: the number of positive instances in each iteration
-		double probability = 0.95; // parameter: the probability of sampling from the model
-		int uncertainbit = 1;      // parameter: the number of samplable dimensions 
+		double probability = 0.99; // parameter: the probability of sampling from the model
+		int uncertainbit = 1;      // parameter: the number of sampled dimensions 
 		
-		// load data for optimimzation ramploss
-		if(false){
-			double c = 1.5;
-			double s = 0.3;
-			String training  = "C:\\Users\\Frank Hu\\Desktop\\ramplosstest.txt";
-			String testing  = "C:\\Users\\Frank Hu\\Desktop\\ramplosstest.txt";
-			RampLoss ramp = new RampLoss(c,s,training,testing);
-			Instance ins = new Instance(ramp.getDim());
-			double fe = 1;
-			ins.setFeature(fe);
-			ins.setValue(ramp.getValue(ins));
-			System.out.println(ins.getValue());
-		}
-		
-		// example for optimization in continuous solutions space
-		if (true) {
+		// example for batch racos in continuous solutions space
+		if (false) {
+			//the default setting of racos is batch
 			Instance ins = null;
 			int repeat = 1;
-			double[] result = new double[repeat];
 			Task t = new Ackley(100);
 			for (int i = 0; i < repeat; i++) {
 				Continue con = new Continue(t);
@@ -43,98 +35,67 @@ public class examples {
 				con.setPositiveNum(positivenum);    // parameter: the number of positive instances in each iteration
 				con.setRandProbability(probability);// parameter: the probability of sampling from the model
 				con.setUncertainBits(uncertainbit); // parameter: the number of samplable dimensions 
-				con.run();                          // call Racos
+				con.run();                          // call batch Racos
 				ins = con.getOptimal();             // obtain optimal
-				result[i] = ins.getValue();
+				System.out.println("best function value:" + ins.getValue());
 			}
-			double sum = 0;
+		}
+		
+		// example for sequential racos in continuous solution space
+		if (true) {
+			Instance ins = null;
+			int repeat = 1;
+			Task t = new Ackley(100);
 			for (int i = 0; i < repeat; i++) {
-				sum = sum + result[i];
+				Continue con = new Continue(t);
+				con.TurnOnSequentialRacos();
+				con.setSampleSize(samplesize);      // parameter: the number of samples in each iteration
+				con.setBudget(budget);              // parameter: the budget of sampling
+				con.setPositiveNum(positivenum);    // parameter: the number of positive instances in each iteration
+				con.setRandProbability(probability);// parameter: the probability of sampling from the model
+				con.setUncertainBits(uncertainbit); // parameter: the number of samplable dimensions 
+				con.run();                          // call sequential Racos
+				ins = con.getOptimal();             // obtain optimal
+				System.out.println("best function value:" + ins.getValue());
 			}
-			double average = sum / repeat;
-			sum = 0;
-			for (int i = 0; i < repeat; i++) {
-				sum = sum + (result[i] - average) * (result[i] - average);
-			}
-			double variance = sum / repeat;
-			System.out.println("Average:" + average + " Variance:" + variance);
 		}
 		
 		// example for optimization in discrete solution space
 		if(false){
 			Instance ins = null;
 			int repeat = 1;
-			double[] result = new double[repeat];
 			Task t = new SetCover(30, 20);
-			// compare with random search
-			{
-				UniformSearch us = new UniformSearch(t);
-				us.setSampleSize(samplesize);  // parameter: the number of samples in each iteration
-				us.setMaxIteration(iteration); // parameter: the number of iterations
-				// us.run();
-				// ins = us.getOptimal();
-				// System.out.println("Uniform Search:"+ins.getValue());
-			}
 			for (int i = 0; i < repeat; i++) {
-				Discrete con = new Discrete(t);
-				con.setSampleSize(samplesize);       // parameter: the number of samples in each iteration
-				con.setMaxIteration(iteration);      // parameter: the number of iterations
-				con.setPositiveNum(positivenum);     // parameter: the number of positive instances in each iteration
-				con.setRandProbability(probability); // parameter: the probability of sampling from the model
-				con.setUncertainBits(uncertainbit);  // parameter: the number of samplable dimensions 
-				con.run();                           // call Racos
-				ins = con.getOptimal();              // obtain optimal
-				result[i] = ins.getValue();
+				Discrete discrete = new Discrete(t);
+				discrete.setSampleSize(samplesize);       // parameter: the number of samples in each iteration
+				discrete.setMaxIteration(iteration);      // parameter: the number of iterations
+				discrete.setPositiveNum(positivenum);     // parameter: the number of positive instances in each iteration
+				discrete.setRandProbability(probability); // parameter: the probability of sampling from the model
+				discrete.setUncertainBits(uncertainbit);  // parameter: the number of samplable dimensions 
+				discrete.run();                           // call Racos
+				ins = discrete.getOptimal();              // obtain optimal
+				System.out.println("best function value:" + ins.getValue());
 			}
-			double sum = 0;
-			for (int i = 0; i < repeat; i++) {
-				sum = sum + result[i];
-			}
-			double average = sum / repeat;
-			sum = 0;
-			for (int i = 0; i < repeat; i++) {
-				sum = sum + (result[i] - average) * (result[i] - average);
-			}
-			double variance = sum / repeat;
-			System.out.println("Average:" + average + " Variance:" + variance);
 		}
 		
 		// example for optimization in mixed solutions space
 		if(false){
 			Instance ins = null;
 			int repeat = 10;
-			double[] result = new double[repeat];
 			Task t = new MixedFunction(100);
-			// us.run();
-			// ins = us.getOptimal();
-			// System.out.println("Uniform Search:"+ins.getValue());
 			for (int i = 0; i < repeat; i++) {
-				Mix con = new Mix(t);
-				con.setSampleSize(samplesize);      // parameter: the number of samples in each iteration
-				con.setMaxIteration(iteration);     // parameter: the number of iterations
-				con.setPositiveNum(positivenum);    // parameter: the number of positive instances in each iteration
-				con.setRandProbability(probability);// parameter: the probability of sampling from the model
-				con.setUncertainBits(uncertainbit); // parameter: the number of samplable dimensions 
-				con.run();                          // call Racos
-				ins = con.getOptimal();             // obtain optimal
-				result[i] = ins.getValue();
-//				con.PrintTime();
-			}
-			double sum = 0;
-			for (int i = 0; i < repeat; i++) {
-				sum = sum + result[i];
-			}
-			double average = sum / repeat;
-			sum = 0;
-			for (int i = 0; i < repeat; i++) {
-				sum = sum + (result[i] - average) * (result[i] - average);
-			}
-			double variance = sum / repeat;
-			System.out.println("Average:" + average + " Variance:" + variance);
-			
+				Mix mix = new Mix(t);
+				mix.setSampleSize(samplesize);      // parameter: the number of samples in each iteration
+				mix.setMaxIteration(iteration);     // parameter: the number of iterations
+				mix.setPositiveNum(positivenum);    // parameter: the number of positive instances in each iteration
+				mix.setRandProbability(probability);// parameter: the probability of sampling from the model
+				mix.setUncertainBits(uncertainbit); // parameter: the number of samplable dimensions 
+				mix.run();                          // call Racos
+				ins = mix.getOptimal();             // obtain optimal
+				System.out.println("best function value:" + ins.getValue());
+			}		
 		}
-
-
+		
 	}
 
 }
